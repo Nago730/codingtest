208982d0281c70f821f2a788fd73f4f6c0a0514f
DESKTOP-4N4R3LH\jqk17 <jqk1797@gmail.com>
2024-12-21 22:21:30 +0900

    추억 점수 다른 풀이 구현

--- 추억 점수.js
+++ 추억 점수.js
@@ -18,3 +18,16 @@ function solution(name, yearning, photo) {
     }
     return answer;
 }
+
+// 두번째 풀이
+// map(), reduce(), Arrow함수, 삼항연산자 사용
+
+function solution(name, yearning, photo) {
+    return photo.map(arr => 
+        arr.reduce((acc, cur) => acc + (yearning[name.indexOf(cur)] ?? 0), 0)
+    );
+}
+
+// 화살표 함수에서 {} 사용시 return 필요
+// 삼항연산자 사용시 조건식 ? 참일때 : 거짓일때
+// 삼항연산자 대신 || 사용 가능. indexOf() 값 못찾으면 -1 반환, array[-1]는 undefined 반환
