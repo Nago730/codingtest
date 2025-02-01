62820606f5115dfc2541f3ea1afd9e650df0f358
DESKTOP-4N4R3LH\jqk17 <jqk1797@gmail.com>
2025-02-01 21:11:23 +0900

    52. 직사각형 별찍기 풀이

---
+++ programmers/implementation/52. 직사각형 별찍기.js
@@ -0,0 +1,15 @@
+// 직사각형 별찍기
+// 플랫폼: Programmers
+// 레벨: 1
+// 유형: 구현
+// URL: https://school.programmers.co.kr/learn/courses/30/lessons/12969
+
+// 솔루션 코드
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', data => {
+    const n = data.split(" ");
+    const a = Number(n[0]), b = Number(n[1]);
+    for (let i = 0; i < b; i++) {
+        console.log('*'.repeat(a));
+    }
+});
