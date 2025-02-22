7f3cd7dd140130644b22591f03f06c8ac23c4e0a
Nago730 <jqk1797@gmail.com>
2025-02-22 12:52:31 +0900

    73. 문자열을 정수로 바꾸기 풀이

---
+++ programmers/implementation/73. 문자열을 정수로 바꾸기.js
@@ -0,0 +1,10 @@
+// 문자열을 정수로 바꾸기
+// 플랫폼: Programmers
+// 레벨: 1
+// 유형: 구현
+// URL: https://school.programmers.co.kr/learn/courses/30/lessons/12925
+
+// 솔루션 코드
+function solution(s) {
+    return +s;
+}
