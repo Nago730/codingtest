61c0261d39ee3ad72949f27eb5091ed626da4eb9
Nago730 <jqk1797@gmail.com>
2025-03-02 23:32:30 +0900

    76. 짝수와 홀수 풀이

---
+++ programmers/implementation/76. 짝수와 홀수.js
@@ -0,0 +1,10 @@
+// 짝수와 홀수수
+// 플랫폼: Programmers
+// 레벨: 1
+// 유형: 구현
+// URL: https://school.programmers.co.kr/learn/courses/30/lessons/12937
+
+// 솔루션 코드
+function solution(num) {
+    return num%2==0 ? 'Even' : 'Odd';
+}
