763445c91d43f86a0abdbe17efd9a23be011a900
Nago <jqk1797@gmail.com>
2025-07-16 14:37:51 +0900

    Time: 60 ms (13.18%), Space: 53.6 MB (97.43%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -3,18 +3,14 @@
  * @return {number}
  */
 var reverse = function(x) {
-    let OldVal;
-    if(x < 0) OldVal = x;
-    x = Math.abs(x);
-    let s = 0;
-    while(x >0) {
-        let r = x%10;
-        s = r + s*10;
-        x = Math.floor(x/10);
+    const sign = x >= 0 ? 1 : -1;
+    x = x >= 0 ? x : -x;
+    let answer = 0;
+    while(x) {
+        answer *= 10;
+        answer += x % 10;
+        x = Math.floor(x / 10);
+        if (answer < -(2**31) || answer > 2**31 - 1) return 0;
     }
-    if(OldVal < 0) s = -s;
-    const initMin = -Math.pow(2, 31);
-    const initMax = Math.pow(2, 31) - 1;
-    if(s < initMin || s > initMax) return 0;
-    return s;
+    return answer * sign;
 };
