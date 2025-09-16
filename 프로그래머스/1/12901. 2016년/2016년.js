34beacc7167226b77b187a989e1df69594e61107
Nago <jqk1797@gmail.com>
2025-09-16 17:42:21 +0900

    [level 1] Title: 2016년, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -0,0 +1,12 @@
+function solution(a, b) {
+    const md = {1: 31, 2: 29, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31};
+    const day = {0: 'FRI', 1: 'SAT', 2: 'SUN', 3: 'MON', 4: 'TUE', 5: 'WED', 6: 'THU'};
+    let days = 0;
+    
+    for (let i = 2; i <= a; i++) {
+        days += md[i - 1];
+    }
+    days += b - 1;
+    
+    return day[days % 7];
+}
